ab8162009f1264d2740e47a400ac93467f12cbee
tanishp1 <pawartanish73@gmail.com>
2026-03-01 14:33:24 +0530

    gmail clone

---
+++ 2 MONTH FEB/React/gmail-clone/src/App.jsx
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1 className='text-center bg-danger'>HOC</h1>
+
+      <p>About us | Related Links | Site Map | Privacy Policy | Grievence Policy 
+        | Refund Policy | Contact us Use Internet Explorer Version 10.0 and above. 
+        Screen resolution 1024 X 768 pixel
+      </p>
+      <p>About us | Related Links | Site Map | Privacy Policy | Grievence Policy 
+        | Refund Policy | Contact us Use Internet Explorer Version 10.0 and above. 
+        Screen resolution 1024 X 768 pixel
+      </p>
+      <p>About us | Related Links | Site Map | Privacy Policy | Grievence Policy 
+        | Refund Policy | Contact us Use Internet Explorer Version 10.0 and above. 
+        Screen resolution 1024 X 768 pixel
+      </p>
+      <p>About us | Related Links | Site Map | Privacy Policy | Grievence Policy 
+        | Refund Policy | Contact us Use Internet Explorer Version 10.0 and above. 
+        Screen resolution 1024 X 768 pixel
+      </p>
+
+      <ul>
+        <li>
+          Home
+        </li>
+        <li>
+          About us 
+        </li>
+        <li>
+          Contact us 
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default App

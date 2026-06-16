a515c819035295a27c16ee65a86c3f20e87eee94
tanishp1 <pawartanish73@gmail.com>
2026-06-16 17:59:58 +0530

    Updated layout and page components

--- 5 MONTH MAY/my-app/app/page.tsx
+++ 5 MONTH MAY/my-app/app/page.tsx
@@ -1,19 +1,22 @@
 "use client"
-import Callback from "./components/Callback";
-import Reducer from "./components/Reducer";
-import Reference from "./components/Reference";
+import { useContextData } from "@/app/components/context/Rootcontext"
+// import Callback from "./components/Callback";
+// import Reducer from "./components/Reducer";
+// import Reference from "./components/Reference";
 // import Counter from "./components/Counter";
 // import Timer from "./components/Timer";
 // import useCustom from "./components/useCustom";
 
 export default function Home(){
   // const { count, increment, decrement, reset} = useCustom(10);
+  const contextData = useContextData()
 
   return(
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <Reducer/>
-      <Reference/>
-      <Callback/>
+     Client Route - Name: {contextData.name}
+      {/* <Reducer/> */}
+      {/* <Reference/> */}
+      {/* <Callback/> */}
       {/* <Counter/>
       <Timer/> */}
 
